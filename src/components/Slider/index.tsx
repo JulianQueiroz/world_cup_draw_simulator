73294acc31f9da8d9a61235e9404a967025164cd
Juliana Queiroz <julianatko@gmail.com>
2026-03-26 01:32:05 -0300

    feat: sliders component ---> groups and teams

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,11 +1,18 @@
 import { Slider } from "../ui/slider";
 import { SliderContent } from "./style";
 
-const SliderComponent = () => {
+type Props = {
+    title:string;
+    sliderValue: number[]
+    onChange: (value: number[]) => void
+}
+
+
+const SliderComponent = ({title,sliderValue, onChange}:Props) => {
     return(
         <SliderContent>
-            <h1>Configuração dos grupos</h1>
-            <Slider defaultValue={[75]} max={100} step={1} className="mx-auto w-full max-w-xs" />
+            <h1>{title}</h1>
+            <Slider  value={sliderValue} onValueChange={onChange} step={1} min={1} max={16} className="mx-auto w-full max-w-xs" />
         </SliderContent>
     )
 };
