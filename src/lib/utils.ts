1f8429bdc7ce4307a491491aba2e936224c810c5
Juliana Queiroz <julianatko@gmail.com>
2026-03-30 17:45:08 -0300

    refactor: utils code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export default function shuffleArray<T>(array: T[]) {
+export function shuffleArray<T>(array: T[]) {
   const copied = [...array];
 
   for (let i = copied.length - 1; i > 0; i--) {
