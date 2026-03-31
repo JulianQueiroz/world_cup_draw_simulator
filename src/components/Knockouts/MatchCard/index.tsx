8cb1b50150e24665bcdcc5919f338aae66b92884
Juliana Queiroz <julianatko@gmail.com>
2026-03-31 19:13:33 -0300

    feat: update MatchCard component with styled components and improve layout

--- src/components/Knockouts/MatchCard/index.tsx
+++ src/components/Knockouts/MatchCard/index.tsx
@@ -1,8 +1,13 @@
-import { Card, CardContent } from '../../ui/card';
-import { cn } from '../../../lib/utils';
-import { Match } from '../../../types/knockout';
 import { TrophyIcon } from 'lucide-react';
 import ReactCountryFlag from 'react-country-flag';
+import { Match } from '../../../types/knockout';
+import {
+  MatchWrapper,
+  TeamButton,
+  TeamInfo,
+  TeamName,
+  ChampionIconWrapper,
+} from './style';
 
 type MatchCardProps = {
   match: Match;
@@ -11,45 +16,58 @@ type MatchCardProps = {
 };
 
 export default function MatchCard({ match, onSelectWinner, isLastRound }: MatchCardProps) {
-  const getTeamHighlight = (teamId: string | null, winner: string | null) => {
-    if (!winner || !teamId) return 'bg-muted';
-    return teamId === winner ? 'bg-green-200 font-bold dark:bg-green dark:text-black' : 'bg-muted';
+  const isWinner = (teamId: string | null, winner: string | null) => {
+    return !!teamId && !!winner && teamId === winner;
   };
-  const isChampion = (teamId: string | null) => isLastRound && !!match.winner && teamId === match.winner;
+
+  const isChampion = (teamId: string | null) =>
+    isLastRound && !!match.winner && teamId === match.winner;
 
   return (
-    <Card className="w-full">
-      <CardContent className="p-0 bg-white shadow-sm border border-gray-200 dark:border-0">
-        {/* time 1 */}
-        <button
-          onClick={() => match.team1.id && onSelectWinner(match.id, match.team1.id)}
-          className={cn('w-full px-4 py-3 text-left divide-y divide-gray-100', getTeamHighlight(match.team1.id, match.winner))}>
-          <div className="flex justify-between">
-            <div>
-              <span className="mr-2">
-                {match.team1.iso && <ReactCountryFlag countryCode={match.team1.iso} svg style={{ width: '1.2em', height: '1.2em' }} />}
-              </span>
-              {match.team1.name}
-            </div>
-            {isChampion(match.team1.id) && <TrophyIcon />}
-          </div>
-        </button>
+    <MatchWrapper>
+      <TeamButton
+        onClick={() => match.team1.id && onSelectWinner(match.id, match.team1.id)}
+        $isWinner={isWinner(match.team1.id, match.winner)}
+      >
+        <TeamInfo>
+          {match.team1.iso && (
+            <ReactCountryFlag
+              countryCode={match.team1.iso}
+              svg
+              style={{ width: '1.2em', height: '1.2em' }}
+            />
+          )}
+          <TeamName>{match.team1.name}</TeamName>
+        </TeamInfo>
 
-        {/* time 2 */}
-        <button
-          onClick={() => match.team2.id && onSelectWinner(match.id, match.team2.id)}
-          className={cn('w-full px-4 py-3 text-left divide-y divide-gray-100', getTeamHighlight(match.team2.id, match.winner))}>
-          <div className="flex justify-between">
-            <div>
-              <span className="mr-2">
-                {match.team2.iso && <ReactCountryFlag countryCode={match.team2.iso} svg style={{ width: '1.2em', height: '1.2em' }} />}
-              </span>
-              {match.team2.name}
-            </div>
-            {isChampion(match.team2.id) && <TrophyIcon />}
-          </div>
-        </button>
-      </CardContent>
-    </Card>
+        {isChampion(match.team1.id) && (
+          <ChampionIconWrapper>
+            <TrophyIcon size={18} />
+          </ChampionIconWrapper>
+        )}
+      </TeamButton>
+
+      <TeamButton
+        onClick={() => match.team2.id && onSelectWinner(match.id, match.team2.id)}
+        $isWinner={isWinner(match.team2.id, match.winner)}
+      >
+        <TeamInfo>
+          {match.team2.iso && (
+            <ReactCountryFlag
+              countryCode={match.team2.iso}
+              svg
+              style={{ width: '1.2em', height: '1.2em' }}
+            />
+          )}
+          <TeamName>{match.team2.name}</TeamName>
+        </TeamInfo>
+
+        {isChampion(match.team2.id) && (
+          <ChampionIconWrapper>
+            <TrophyIcon size={18} />
+          </ChampionIconWrapper>
+        )}
+      </TeamButton>
+    </MatchWrapper>
   );
 }
